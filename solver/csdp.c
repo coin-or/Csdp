6443784b502c3e27628d0ab17c8fd19941e9979f
borchers@nmt.edu <borchers@nmt.edu>
2010-04-09 01:03:15 +0000

    Updated version number to 6.1.1.

--- solver/csdp.c
+++ solver/csdp.c
@@ -54,7 +54,7 @@ int main(argc,argv)
 
   if ((argc < 2) || (argc > 4)) 
     {
-      printf("CSDP 6.1.0\n");
+      printf("CSDP 6.1.1\n");
       printf(" \n");
       printf("Usage: \n");
       printf("\n");
