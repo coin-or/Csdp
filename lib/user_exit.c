316e6717947e0297779728a39c6236caeb8d4ac8
borchers@nmt.edu <borchers@nmt.edu>
2017-07-21 16:32:48 +0000

    Updated user_exit() handling of SIGXCPU to add time to finish the last iteration.

--- lib/user_exit.c
+++ lib/user_exit.c
@@ -10,6 +10,8 @@
 
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <signal.h>
 
 int sigterm_signaled=0;
@@ -22,7 +24,32 @@ int sigterm_signaled=0;
 void catch_sigterm(signal)
      int signal;
 {
+  printf("Process stopped by signal.\n");
   sigterm_signaled=1;
+}
+
+/*
+ * This version is used for the sigxcpu signal to give us more time. 
+ */
+
+void catch_sigxcpu(signal)
+     int signal;
+{
+  int ret;
+  struct rlimit rlim;
+
+  printf("CPU Time Limit Execeded.\n");
+  sigterm_signaled=1;
+
+  /*
+   * Update the time limit.
+   */
+  
+  ret=getrlimit(RLIMIT_CPU,&rlim);
+  rlim.rlim_cur=rlim.rlim_cur+200+rlim.rlim_cur/10;
+
+  ret=setrlimit(RLIMIT_CPU,&rlim);
+
 }
 
 int user_exit(n,k,C,a,dobj,pobj,constant_offset,constraints,X,y,Z,params)
@@ -41,12 +68,16 @@ int user_exit(n,k,C,a,dobj,pobj,constant_offset,constraints,X,y,Z,params)
 {
   /*
    * Stop on any of the following signals.  SIGTERM, SIGXCPU, SIGINT, SIGQUIT
+   * SIGTERM, SIGINT, SIGQUIT all set a flag to tell the code to exit at the
+   * end of the current iteration.  SIGXCPU's handler also extends the time 
+   * limit to give us the time to do this.  
    */
   
   signal(SIGTERM,catch_sigterm);
-  signal(SIGXCPU,catch_sigterm);
   signal(SIGINT,catch_sigterm);
   signal(SIGQUIT,catch_sigterm);
+  signal(SIGXCPU,catch_sigxcpu);
+
   
   /*
    * If a signal to terminate has been raised, then quit.
