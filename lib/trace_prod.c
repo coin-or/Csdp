c25b837773ddb2fdac3e3dbbbbf6820bb85eb715
borchers@nmt.edu <borchers@nmt.edu>
2018-06-04 15:59:10 +0000

    Updated trace_prod to access only the upper triangle of A and B and to use OpenMP.

--- lib/trace_prod.c
+++ lib/trace_prod.c
@@ -1,7 +1,14 @@
 /*
-  Compute the trace of the product of two matrices.  Since we 
-  only need the trace, it makes more sense to just compute the diagonal
-  of the product and sum the entries..
+  Compute the trace of the product of two symmetric matrices.  Since this
+  computation is very memory bound, we exploit the symmetry to cut the 
+  memory accesses in half.  The basic formula is that 
+ 
+  tr(AB)=sum(sum(A(i,j)*B(i,j),i=1..n),j=1..n)
+
+  or more efficiently 
+
+  tr(AB)=2*sum(sum(A(i,j)*B(i,j),i=1..j-1),j=1..n)+sum(A(i,i)*B(i,i),i=1..n)
+
   */
 
 #include <stdlib.h>
@@ -14,6 +21,7 @@ double trace_prod(A,B)
 {
   int blk;
   double sum;
+  double temp;
   int i;
   int j;
 
@@ -27,10 +35,16 @@ double trace_prod(A,B)
 	    sum += A.blocks[blk].data.vec[i]*B.blocks[blk].data.vec[i];
 	  break;
 	case MATRIX:
+	  temp=0;
+#pragma omp parallel for schedule(dynamic,64) default(none) private(i,j) shared(A,B,blk) reduction(+:temp)
 	  for (j=1; j<=A.blocks[blk].blocksize; j++)
-	    for (i=1; i<=A.blocks[blk].blocksize; i++)
-	      sum += A.blocks[blk].data.mat[ijtok(i,j,A.blocks[blk].blocksize)]
-                    *B.blocks[blk].data.mat[ijtok(j,i,A.blocks[blk].blocksize)];
+	    for (i=1; i<j; i++)
+	      temp=temp+A.blocks[blk].data.mat[ijtok(i,j,A.blocks[blk].blocksize)]
+		*B.blocks[blk].data.mat[ijtok(i,j,A.blocks[blk].blocksize)];
+	  sum += 2.0*temp;
+	  for (i=1; i<=A.blocks[blk].blocksize; i++)
+	      sum += A.blocks[blk].data.mat[ijtok(i,i,A.blocks[blk].blocksize)]
+                    *B.blocks[blk].data.mat[ijtok(i,i,A.blocks[blk].blocksize)];
 	  break;
 	case PACKEDMATRIX:
 	default:
