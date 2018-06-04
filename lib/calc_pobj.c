e55a726a3b7c6565755c6cf646e864344836ad53
borchers@nmt.edu <borchers@nmt.edu>
2018-06-04 15:49:59 +0000

    Updated calc_pobj.c to just use trace_prod.

--- lib/calc_pobj.c
+++ lib/calc_pobj.c
@@ -17,28 +17,7 @@ double calc_pobj(C,X,constant_offset)
   int i,j;
   double pobj;
 
-  pobj=constant_offset;
-
-  for (blk=1; blk<=C.nblocks; blk++)
-    {
-      switch (C.blocks[blk].blockcategory)
-	{
-	case DIAG:
-	  for (i=1; i<=C.blocks[blk].blocksize; i++)
-	    {
-	      pobj += C.blocks[blk].data.vec[i]*X.blocks[blk].data.vec[i];
-	    };
-	  break;
-	case MATRIX:
-	  for (j=1; j<=C.blocks[blk].blocksize; j++)
-	    for (i=1; i<=C.blocks[blk].blocksize; i++)
-	      pobj += C.blocks[blk].data.mat[ijtok(i,j,C.blocks[blk].blocksize)]*X.blocks[blk].data.mat[ijtok(j,i,C.blocks[blk].blocksize)];
-          break;
-	default:
-	  printf("calc_pobj illegal block type!\n");
-	  exit(206);
-	};
-    };
+  pobj=constant_offset+trace_prod(C,X);
 
   return(pobj);
 	 
