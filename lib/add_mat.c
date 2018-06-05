cf74aca3ed911643cd0e2f1e94014d062b6854ab
borchers@nmt.edu <borchers@nmt.edu>
2018-06-05 17:27:03 +0000

    More parallelization

--- lib/add_mat.c
+++ lib/add_mat.c
@@ -18,13 +18,16 @@ void add_mat(A,B)
       switch (A.blocks[blk].blockcategory)
 	{
 	case DIAG:
+#pragma omp simd
 	  for (i=1; i<=A.blocks[blk].blocksize; i++)
 	    {
 	      B.blocks[blk].data.vec[i]+=A.blocks[blk].data.vec[i];
 	    };
 	  break;
 	case MATRIX:
+#pragma omp parallel for schedule(dynamic,64) default(none) private(i,j) shared(A,B,blk)
 	  for (j=1; j<=A.blocks[blk].blocksize; j++)
+#pragma omp simd
 	    for (i=1; i<=A.blocks[blk].blocksize; i++)
 	      B.blocks[blk].data.mat[ijtok(i,j,B.blocks[blk].blocksize)] +=
 		A.blocks[blk].data.mat[ijtok(i,j,A.blocks[blk].blocksize)];
